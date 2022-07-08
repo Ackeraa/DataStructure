492cbb2da49a13da43e62c85bbfbfc3a8188ed8b
Ackeraa <ggaoshanxun@gmail.com>
2022-07-08 18:52:20 +0800

    added some fig

--- AC-Automata/manim_plot/main.py
+++ AC-Automata/manim_plot/main.py
@@ -2,11 +2,12 @@ from manim import *
 from utils import *
 import math
 import numpy as np
+import queue
+import random
 
 class Trie(Scene):
     def construct(self):
-        #self.camera.background_color = WHITE
-        self.add_sound("bgm.mp3")
+        self.camera.background_color = WHITE
         self.patterns = ["ab", "about", "at", "ate", "be", "bed", "edge", "get"]
         self.root = TrieNode()
         self.nodes = VGroup(self.root)
@@ -15,7 +16,7 @@ class Trie(Scene):
         self.place(self.root)
         # self.add(self.edges)
         #self.add(self.nodes)
-        self.build_animate()
+        #self.build_animate()
         self.match_animate()
         self.wait()
 
@@ -33,6 +34,7 @@ class Trie(Scene):
                     self.nodes.add(v)
                 u = u.children[c]
             u.pattern = pattern
+            self.nodes[-1][0].set_fill(RED_E, opacity=1)
 
     def place(self, father):
         n = max(1, len(father.children))
@@ -54,7 +56,7 @@ class Trie(Scene):
             dp2 = np.array(p1 - p2)
             lp = math.sqrt(dp2[0] ** 2 + dp2[1] ** 2)
             dp2 = dp2 / lp * child[0].radius
-            e = Line(p1+dp1, p2+dp2, color=WHITE)
+            e = Line(p1+dp1, p2+dp2, color=BLACK)
             self.edges.add(e)
             self.place(child)
 
@@ -63,7 +65,7 @@ class Trie(Scene):
         root = TrieNode()
         patterns = VGroup()
         for pattern in self.patterns:
-            text = Text(pattern, font="DroidSansMono Nerd Font", font_size=22)
+            text = Text(pattern, color=BLACK, font="DroidSansMono Nerd Font", font_size=22)
             patterns.add(text)
         patterns.arrange(DOWN, buff=0.5)
         patterns.shift(RIGHT*5)
@@ -98,11 +100,11 @@ class Trie(Scene):
                     dp2 = np.array(p1 - p2)
                     lp = math.sqrt(dp2[0] ** 2 + dp2[1] ** 2)
                     dp2 = dp2 / lp * v[0].radius
-                    e = Line(p1+dp1, p2+dp2, color=WHITE)
+                    e = Line(p1+dp1, p2+dp2, color=BLACK)
                     self.play(
                             ReplacementTransform(u0.copy(), v),
                             Create(e),
-                        )
+                            )
                 else:
                     self.play(Indicate(u0.children[c][0], color=TEAL))
 
@@ -119,16 +121,13 @@ class Trie(Scene):
         texts = ["ate", "abou", "abc"]
         texts_vg = VGroup()
         for text in texts:
-            t = Text(text, font="DroidSansMono Nerd Font", font_size=22)
+            t = Text(text, color=BLACK, font="DroidSansMono Nerd Font", font_size=22)
             texts_vg.add(t)
         texts_vg.arrange(DOWN, buff=0.5)
         texts_vg.shift(RIGHT*5+UP*1.5)
 
         title = Text("查询", font_size=26, color=BLUE_E).shift(UP*3.5+RIGHT*5)
-        self.play(
-                self.title.animate.become(title),
-                ReplacementTransform(self.patterns_vg, texts_vg),
-            )
+        self.add(self.nodes, self.edges, texts_vg, title)
 
         ar = Triangle(color=RED_E).set_fill(RED, opacity=1).rotate(-180*DEGREES).scale(.03)
         ar.move_to(texts_vg[0]).shift(UP*0.3+LEFT*(len(texts[0])//2)*0.2)
@@ -177,29 +176,179 @@ class Trie(Scene):
                     pattern = u.pattern
                     if pattern is not None:
                         print(pattern)
-                        
-class Test(Scene):
+
+class ACAutomata(Scene):
     def construct(self):
-        self.patterns = ["ab", "about", "at", "ate", "be", "bed", "edge", "get"]
+        self.camera.background_color = WHITE
+        self.patterns = ["i", "in", "tin", "sting"]
+        self.text = "sting"
+        #self.patterns = ["at", "art", "oars", "soar"]
+        #self.text = "soat"
+        self.root = TrieNode()
+        self.nodes = VGroup(self.root)
+        self.edges = VGroup()
+        self.suffix_links = VGroup()
+        self.output_links = VGroup()
+        self.build()
+        self.place(self.root)
+        self.bfs()
+        self.add(self.edges)
+        self.add(self.nodes)
+        self.add(self.suffix_links)
+        self.add(self.output_links)
         patterns = VGroup()
         for pattern in self.patterns:
-            text = Text(pattern, font="DroidSansMono Nerd Font", font_size=22)
+            text = Text(pattern, color=BLACK, font="DroidSansMono Nerd Font", font_size=22)
             patterns.add(text)
         patterns.arrange(DOWN, buff=0.5)
-        patterns.shift(RIGHT*5)
         self.add(patterns)
-        ar = Triangle(color=RED_E).set_fill(RED, opacity=1).rotate(-180*DEGREES).scale(.03)
-        i = 1
-        ar.move_to(patterns[i])
-        ar.shift(UP*0.3+LEFT*(len(self.patterns[i])//2) * 0.2)
-        if len(self.patterns[i]) % 2 == 0:
-            ar.shift(RIGHT*0.1)
-        for i in range(4):
-            self.play(ar.animate.shift(RIGHT*0.2), run_time=0.8)
+        patterns.shift(RIGHT*5)
+        self.nodes.shift(LEFT)
+        self.edges.shift(LEFT)
+        self.suffix_links.shift(LEFT)
+        self.output_links.shift(LEFT)
+        self.matches()
+        #self.build_animate()
+        #self.match_animate()
+        #self.wait()
 
+    def build(self):
+        w = 10
+        self.root.w = w
+        self.root.l = -w // 2 
+        self.root.shift(UP*3)
+        for pattern in self.patterns:
+            u = self.root
+            for c in pattern:
+                if c not in u.children:
+                    v = TrieNode(c, len(u.children))
+                    u.children[c] = v 
+                    v.dth = u.dth + 1
+                    self.nodes.add(v)
+                u = u.children[c]
+            u.pattern = pattern
+            self.nodes[-1][0].set_fill(RED_E, opacity=1)
 
-        self.add(ar)
+    def bfs(self):
+        q = queue.Queue()
+        q.put(self.root)
+        while not q.empty():
+            u = q.get()
+            # Build suffix link.
+            for a, v in u.children.items():
+                if u == self.root:
+                    v.suffix_link = u
+                else:
+                    x = u.suffix_link
+                    while v.suffix_link is None:
+                        if a in x.children.keys():
+                            v.suffix_link = x.children[a]
+                        elif x == self.root:
+                            v.suffix_link = x
+                        else:
+                            x = x.suffix_link
 
+                p1 = v.get_center()
+                p2 = v.suffix_link.get_center()
+                np1, np2 = get_p(p1, p2, v[0].radius)
+                p1[1] += 0.1
+                p2[1] += 0.1
+                l = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+                e = Arrow(p1, p2, color=RED_C, stroke_width=3, 
+                        max_tip_length_to_length_ratio=0.2/l)
+                self.suffix_links.add(e)
+                v.suffix_edge = e
 
-        self.wait()
+                # Build output link.
+                if v.suffix_link.pattern is not None:
+                    v.output_link = v.suffix_link
+                else:
+                    v.output_link = v.suffix_link.output_link
+                if v.output_link is not None:
+                    p1 = v.get_center()
+                    p2 = v.output_link.get_center()
+                    np1, np2 = get_p(p1, p2, v[0].radius)
+                    rand = random.random() * 0.1
+                    p1[1] -= 0.15 + rand
+                    p2[1] -= 0.15 + rand
+                    l = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+                    e = Arrow(p1, p2, color=BLUE_E, stroke_width=3, 
+                            max_tip_length_to_length_ratio=0.2/l)
+                    self.output_links.add(e)
+                    v.output_edge = e
 
+                q.put(v)
+
+    def place(self, father):
+        n = max(1, len(father.children))
+        l = father.l
+        w = father.w
+        h = father.get_center()[1]
+        gap = w / n / 2 
+        p1 = father.get_center()
+        for child in father.children.values():
+            pos = [l + (child.idx * 2 + 1) * gap, h - 1.3, 0]
+            child.l = pos[0] - gap
+            child.w = 2 * gap
+            child.move_to(pos)
+            p2 = child.get_center()
+            np1, np2 = get_p(p1, p2, child[0].radius)
+            e = Line(np1, np2, color=BLACK)
+            self.edges.add(e)
+            self.place(child)
+
+    def matches(self):
+        text = Array(self.text)
+        text.shift(DOWN*3.3+LEFT)
+        self.add(text)
+        ari = Triangle(color=RED_E).set_fill(RED, opacity=1).rotate(-180*DEGREES).scale(.03)
+        pi = Text("i", color=RED_E, font="DroidSansMono Nerd Font", font_size=16)
+        pi.next_to(text[0], UP, buff=0.1).shift(LEFT*0.2)
+        pj = Text("j", color=RED_E, font="DroidSansMono Nerd Font", font_size=16)
+        pj.next_to(text[0], DOWN, buff=0.1).shift(LEFT*0.2)
+        self.add(pi, pj)
+
+        u = self.root
+
+        i = 0
+        j = 0
+        for c in self.text:
+            while not c in u.children.keys():
+                e = u.suffix_edge
+                i = j - u.suffix_link.dth
+                u = u.suffix_link
+                run_time = 0.8
+                self.play(ShowPassingFlash(e.copy().set_color(BLUE_E),
+                    run_time=run_time,
+                    time_width=run_time))
+                anim  = pi.animate.next_to(text[i], UP, buff=0.1)
+                self.play(anim, Indicate(u[0], color=BLUE_E), run_time=1)
+                if u == self.root:
+                    break
+            if c in u.children.keys():
+                u = u.children[c]
+                anim  = pj.animate.next_to(text[j], DOWN, buff=0.1)
+                j += 1
+                self.play(anim, Indicate(u[0], color=BLUE_E), run_time=1)
+            v = u
+            while v is not None:
+                if v.pattern is not None:
+                    anims = []
+                   for k in range(i, j):
+                       anims.append(Indicate(text[k][1], color=RED_E))
+                   self.play(*anims)
+                v = v.output_link
+
+class Test(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        text = "soat"
+        self.text = Array(text)
+        self.text.shift(DOWN*3)
+        self.add(self.text)
+        ari = Triangle(color=RED_E).set_fill(RED, opacity=1).rotate(-180*DEGREES).scale(.03)
+        pi = Text("i", color=RED_E, font="DroidSansMono Nerd Font", font_size=16)
+        pi.next_to(self.text[0], UP, buff=0.1)
+        pj = Text("j", color=RED_E, font="DroidSansMono Nerd Font", font_size=16)
+        pj.next_to(self.text[2], DOWN, buff=0.1)
+        self.add(pi, pj)
--- AC-Automata/manim_plot/utils.py
+++ AC-Automata/manim_plot/utils.py
@@ -1,12 +1,39 @@
 from manim import *
+import math
+
+class Array(VGroup):
+
+    def __init__(self, values, square_size=0.6, color=BLACK):
+        size = len(values)
+        super().__init__()
+        for i in range(size):
+            sq = Square(square_size, color=BLACK, stroke_width=2)
+            txt = Text(str(values[i]), color=color, font="DroidSansMono Nerd Font", font_size=24).scale(0.6)
+            txt.move_to(sq.get_center())
+            self.add(VGroup(sq, txt))
+        self.arrange(RIGHT, buff=0)
 
 class TrieNode(VGroup):
     
-    def __init__(self, value="", idx=0, size=0.3, node_color=WHITE, text_color=WHITE):
+    def __init__(self, value="", idx=0, size=0.3, node_color=BLACK, text_color=BLACK):
+        self.suffix_link = None
+        self.output_link = None
+        self.suffix_edge = None
+        self.output_edge = None
         self.children = {}
+        self.dth = 0
         self.idx = idx
         self.pattern = None
-        self.node = Circle(size).set_color(node_color).set_fill(BLACK, opacity=1)
+        self.node = Circle(size).set_color(node_color).set_fill(WHITE, opacity=1)
         self.value = Text(value, color=text_color, font="DroidSansMono Nerd Font").scale(0.6).move_to(self.node.get_center())
         super().__init__(self.node, self.value)
 
+def get_p(p1, p2, radius):
+    dp1 = np.array(p2 - p1)
+    lp = math.sqrt(dp1[0] ** 2 + dp1[1] ** 2)
+    dp1 = dp1 / lp * radius
+    dp2 = np.array(p1 - p2)
+    lp = math.sqrt(dp2[0] ** 2 + dp2[1] ** 2)
+    dp2 = dp2 / lp * radius
+
+    return p1 + dp1, p2 + dp2
